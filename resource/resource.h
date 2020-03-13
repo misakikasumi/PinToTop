fa614746f97bc6b46161565432592963121b3460
Misaki Kasumi <misakikasumi@outlook.com>
2020-03-13 17:44:15 +0800

    initial commit

---
+++ resource/resource.h
@@ -0,0 +1,11 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resource.rc
+//
+#define IDI_APP                         5
+#define IDS_APP_TITLE                   101
+#define IDC_WNDCLASS                    102
+#define IDS_EXIT                        103
+#define IDS_FAIL_INFOTITLE              104
+#define IDS_WND_CLOSED_INFO             105
+#define IDS_WND_ACCESS_DENIED_INFO      106
